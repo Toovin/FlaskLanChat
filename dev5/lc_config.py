18b7dd30e1b8f430b6135781b7a8f055ba0c2295
TOOVIN EXTREME <73458365+Toovin@users.noreply.github.com>
2025-09-13 04:11:47 -0800

    Delete dev5 directory
    redo/reupload

--- dev5/lc_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-UPLOAD_DIR = 'static/uploads'
-AVATAR_DIR = 'static/avatars'
-EXTENSIONS_DIR = 'extensions'
-LIVEKIT_API_KEY = os.getenv('LIVEKIT_API_KEY', 'devkey')  # Default for boilerplate
-LIVEKIT_API_SECRET = os.getenv('LIVEKIT_API_SECRET', 'secret')  # Default for boilerplate
-LIVEKIT_WS_URL = os.getenv('LIVEKIT_WS_URL', 'ws://192.168.1.101:7880')  # Configure for your LiveKit server
